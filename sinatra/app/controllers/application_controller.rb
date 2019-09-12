12eedf96b7c6214f6aedf07eb0b5adc45ebb776e
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-09-12 20:01:18 +0000

    some more progress

--- sinatra/app/controllers/application_controller.rb
+++ sinatra/app/controllers/application_controller.rb
@@ -3,15 +3,15 @@ require_relative '../../config/environment'
 class ApplicationController < Sinatra::Base
 
 
-  configure do 
+  configure do
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
     set  :session_secret, "batman"
-  end 
-  
-  get '/' do 
-    "Hello World!"
-  end 
+  end
+
+  get '/' do
+    erb :index
+  end
 
 end
